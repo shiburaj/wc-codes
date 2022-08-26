be37e0696901dfc34a7c45c86e2b5cc39865f9c7
Shiburaj Pappu <powerupneo@gmail.com>
2022-08-26 08:55:28 +0530

    examples added

---
+++ chapter2/loops.js
@@ -0,0 +1,16 @@
+for (let index = 10; index < 0; index--) {
+    console.log('Hello '+index)
+    
+}
+
+index = 10;
+while (index < 0) {
+    console.log('Hello '+index)
+    index--;
+}
+
+index = 10;
+do {
+    console.log('Hello '+index);
+    index--;
+}while(index < 0);
